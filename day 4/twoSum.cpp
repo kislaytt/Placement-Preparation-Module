ffcf902f84d049df988e5c5a7057333b79c6ef58
kislaytt <98452481+kislaytt@users.noreply.github.com>
2023-07-24 03:28:29 +0530

    Add files via upload

--- day 4/twoSum.cpp
+++ day 4/twoSum.cpp
@@ -1,25 +1,27 @@
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> m;
-        vector<int> ans;
-        int a=0;
-        for(int i =0;i <nums.size(); i++)
-        {
-            int k = (target - nums[i]);
-            if(m[k]>0)
-            {
-                
-                ans.push_back(i);
-                ans.push_back(m[k]-1);
-            }
-            else
-            {
-                
-                m[nums[i]]=i+1;
-                
-            }
-          }
-        return ans;
-    }
-};
+/*
+Find all the pairs with the given sum.
+[Hint: In the HashMap store the occurences of the element]
+
+T.C - O(N)
+S.C - O(N)
+*/
+
+#include <bits/stdc++.h>
+
+vector<vector<int>> pairSum(vector<int> &arr, int s){
+   vector<vector<int>> ans;
+   unordered_map<int, int> mp;
+   
+   for(auto i : arr) {
+      int more = s - i;
+      if(mp.find(more) != mp.end()) {
+         for(int j = 0; j < mp[more]; j++) {
+            ans.push_back({min(i, more), max(i, more)});
+         }
+      }
+      mp[i]++;
+   }
+
+   sort(ans.begin(), ans.end());
+   return ans;
+}
